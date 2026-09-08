623876b109176650e8547915f3a755c20572bc4d
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 02:09:04 -0400

    feat: export composite XML parser

---
+++ src/pssapi/parsing/__init__.py
@@ -0,0 +1,5 @@
+"""Response parsing helpers."""
+
+from .xml import parse_composite_entities, parse_entity, parse_entity_list
+
+__all__ = ["parse_composite_entities", "parse_entity", "parse_entity_list"]
