9b2981bc6cc817b0e8c14ba163c5bea82debea60
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 02:09:19 -0400

    refactor: migrate AllianceService to modern raw client

--- src/pssapi/services/alliance_service.py
+++ src/pssapi/services/alliance_service.py
@@ -7,46 +7,108 @@ from ..entities import Alliance as _Alliance
 from ..entities import Character as _Character
 from ..entities import Message as _Message
 from ..entities import User as _User
-from .raw import AllianceServiceRaw as _AllianceServiceRaw
+from ..parsing.xml import parse_composite_entities as _parse_composite_entities
+from ..parsing.xml import parse_entity as _parse_entity
+from ..parsing.xml import parse_entity_list as _parse_entity_list
 
 
 class AllianceService(_service_base.ServiceBase):
     async def get_alliance(self, access_token: str, alliance_id: int) -> _Alliance:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.get_alliance(production_server, access_token, alliance_id)
-        return result
+        response = await self.client.raw.call(
+            "AllianceService",
+            "GetAlliance",
+            params={"accessToken": access_token, "allianceId": alliance_id},
+            use_cache=False,
+        )
+        return _parse_entity(response, "Alliance", _Alliance)
 
     async def get_user(self, access_token: str, user_id: int) -> _User:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.get_user(production_server, access_token, user_id)
-        return result
+        response = await self.client.raw.call(
+            "AllianceService",
+            "GetUser",
+            params={"accessToken": access_token, "userId": user_id},
+            use_cache=False,
+        )
+        return _parse_entity(response, "User", _User)
 
-    async def list_alliances_by_championship_score_ranking(self, access_token: str, from_: int, to: int) -> _List[_Alliance]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.list_alliances_by_championship_score_ranking(production_server, access_token, from_, to)
-        return result
+    async def list_alliances_by_championship_score_ranking(
+        self, access_token: str, from_: int, to: int
+    ) -> _List[_Alliance]:
+        response = await self.client.raw.call(
+            "AllianceService",
+            "ListAlliancesByChampionshipScoreRanking",
+            params={"accessToken": access_token, "from": from_, "to": to},
+            use_cache=False,
+        )
+        return _parse_entity_list(response, "Alliances", _Alliance)
 
     async def list_alliances_by_ranking(self, skip: int, take: int) -> _List[_Alliance]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.list_alliances_by_ranking(production_server, skip, take)
-        return result
+        response = await self.client.raw.call(
+            "AllianceService",
+            "ListAlliancesByRanking",
+            params={"skip": skip, "take": take},
+            use_cache=False,
+        )
+        return _parse_entity_list(response, "Alliances", _Alliance)
 
     async def list_alliances_with_division(self, division_design_id: int) -> _List[_Alliance]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.list_alliances_with_division(production_server, division_design_id)
-        return result
+        response = await self.client.raw.call(
+            "AllianceService",
+            "ListAlliancesWithDivision",
+            params={"divisionDesignId": division_design_id},
+            use_cache=False,
+        )
+        return _parse_entity_list(response, "Alliances", _Alliance)
 
-    async def list_characters_given_in_alliance(self, access_token: str, alliance_id: int, skip: int, take: int) -> _List[_Character]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.list_characters_given_in_alliance(production_server, access_token, alliance_id, skip, take)
-        return result
+    async def list_characters_given_in_alliance(
+        self, access_token: str, alliance_id: int, skip: int, take: int
+    ) -> _List[_Character]:
+        response = await self.client.raw.call(
+            "AllianceService",
+            "ListCharactersGivenInAlliance",
+            params={
+                "accessToken": access_token,
+                "allianceId": alliance_id,
+                "skip": skip,
+                "take": take,
+            },
+            use_cache=False,
+        )
+        return _parse_entity_list(response, "Characters", _Character)
 
-    async def list_users(self, access_token: str, alliance_id: int, skip: int, take: int) -> _Tuple[_List[_Message], _List[_User]]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.list_users_2(production_server, access_token, alliance_id, skip, take)
-        return result
+    async def list_users(
+        self, access_token: str, alliance_id: int, skip: int, take: int
+    ) -> _Tuple[_List[_Message], _List[_User]]:
+        response = await self.client.raw.call(
+            "AllianceService",
+            "ListUsers2",
+            params={
+                "accessToken": access_token,
+                "allianceId": alliance_id,
+                "skip": skip,
+                "take": take,
+            },
+            use_cache=False,
+        )
+        alliance, users = _parse_composite_entities(
+            response,
+            ("Alliance", _Alliance, False),
+            ("Users", _User, True),
+        )
+        return alliance, users
 
-    async def search_alliances(self, access_token: str, name: str, skip: int, take: int) -> _List[_Alliance]:
-        production_server = await self.get_production_server()
-        result = await _AllianceServiceRaw.search_alliances(production_server, access_token, name, skip, take)
-        return result
+    async def search_alliances(
+        self, access_token: str, name: str, skip: int, take: int
+    ) -> _List[_Alliance]:
+        response = await self.client.raw.call(
+            "AllianceService",
+            "SearchAlliances",
+            params={
+                "accessToken": access_token,
+                "name": name,
+                "skip": skip,
+                "take": take,
+            },
+            use_cache=False,
+        )
+        return _parse_entity_list(response, "Alliances", _Alliance)
