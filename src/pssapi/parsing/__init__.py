a9a45b57be9aef2611f1fe7b553a120a30e61f96
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 02:11:40 -0400

    feat: export expanded XML parsers

--- src/pssapi/parsing/__init__.py
+++ src/pssapi/parsing/__init__.py
@@ -1,5 +1,5 @@
 """Parsing helpers for Pixel Starships API responses."""
 
-from .xml import parse_entity_list
+from .xml import parse_entity, parse_entity_bundle, parse_entity_list
 
-__all__ = ["parse_entity_list"]
+__all__ = ["parse_entity", "parse_entity_bundle", "parse_entity_list"]
