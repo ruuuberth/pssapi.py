fc4e186af9a25dabe32acdb3308930a9306ab15a
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 01:40:01 -0400

    refactor: remove duplicate raw client export

--- src/pssapi/raw/__init__.py
+++ src/pssapi/raw/__init__.py
@@ -1,10 +1,8 @@
 from ..entities import raw as entities
 from ..services import raw as services
-from .client import RawApiClient
 
 
 __all__ = [
     "services",
     "entities",
-    "RawApiClient",
 ]
