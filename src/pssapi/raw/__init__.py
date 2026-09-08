c3281b7ded1d3014bdc6a7fc04138ff2d3b3352d
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 00:46:41 -0400

    Export generic raw API client

--- src/pssapi/raw/__init__.py
+++ src/pssapi/raw/__init__.py
@@ -1,8 +1,10 @@
 from ..entities import raw as entities
 from ..services import raw as services
+from .client import RawApiClient
 
 
 __all__ = [
     "services",
     "entities",
+    "RawApiClient",
 ]
