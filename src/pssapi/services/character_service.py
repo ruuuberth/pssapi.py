3362fcd79cf0e6755b8435d337a13a911d2d14a1
ruuuberth <77597261+ruuuberth@users.noreply.github.com>
2026-09-08 01:53:11 -0400

    refactor: migrate CharacterService to modern transport

--- src/pssapi/services/character_service.py
+++ src/pssapi/services/character_service.py
@@ -8,40 +8,106 @@ from ..entities import CharacterDesign as _CharacterDesign
 from ..entities import CharacterDesignAction as _CharacterDesignAction
 from ..entities import DrawDesign as _DrawDesign
 from ..entities import Prestige as _Prestige
-from .raw import CharacterServiceRaw as _CharacterServiceRaw
 
 
 class CharacterService(_service_base.CacheableServiceBase):
-    async def to_character(self, character_name: str, client_date_time: _datetime.datetime = None, design_version: int = None) -> _List[_CharacterDesign]:
+    async def to_character(
+        self,
+        character_name: str,
+        client_date_time: _datetime.datetime = None,
+        design_version: int = None,
+    ) -> _List[_CharacterDesign]:
         characters = await self.list_all_character_designs(client_date_time, design_version)
-        result = list(filter(lambda character: character_name.lower() in character.character_design_name.lower(), characters))
-
-        return result
+        return [
+            character
+            for character in characters
+            if character_name.lower() in character.character_design_name.lower()
+        ]
 
     @_service_base.cache_endpoint("CharacterDesignActionVersion")
-    async def list_all_character_design_actions(self, client_date_time: _datetime.datetime = None, design_version: int = None) -> _List[_CharacterDesignAction]:
-        production_server = await self.get_production_server()
-        result = await _CharacterServiceRaw.list_all_character_design_actions(production_server, _utils.datetime.convert_to_pss_timestamp(client_date_time), design_version)
-        return result
+    async def list_all_character_design_actions(
+        self,
+        client_date_time: _datetime.datetime = None,
+        design_version: int = None,
+    ) -> _List[_CharacterDesignAction]:
+        params = {
+            "clientDateTime": _utils.datetime.convert_to_pss_timestamp(client_date_time),
+        }
+        if design_version is not None:
+            params["designVersion"] = design_version
+
+        response = await self.client.raw.call(
+            "CharacterService",
+            "ListAllCharacterDesignActions",
+            params=params,
+            use_cache=False,
+        )
+        return _service_base.parse_entity_list(
+            response,
+            "CharacterDesignActions",
+            _CharacterDesignAction,
+        )
 
     @_service_base.cache_endpoint("CharacterDesignVersion")
-    async def list_all_character_designs(self, client_date_time: _datetime.datetime = None, design_version: int = None) -> _List[_CharacterDesign]:
-        production_server = await self.get_production_server()
-        result = await _CharacterServiceRaw.list_all_character_designs_2(production_server, _utils.datetime.convert_to_pss_timestamp(client_date_time), design_version, self.language_key)
-        return result
+    async def list_all_character_designs(
+        self,
+        client_date_time: _datetime.datetime = None,
+        design_version: int = None,
+    ) -> _List[_CharacterDesign]:
+        params = {
+            "clientDateTime": _utils.datetime.convert_to_pss_timestamp(client_date_time),
+            "languageKey": str(self.language_key),
+        }
+        if design_version is not None:
+            params["designVersion"] = design_version
+
+        response = await self.client.raw.call(
+            "CharacterService",
+            "ListAllCharacterDesigns2",
+            params=params,
+            use_cache=False,
+        )
+        return _service_base.parse_entity_list(
+            response,
+            "CharacterDesigns",
+            _CharacterDesign,
+        )
 
     @_service_base.cache_endpoint("DrawDesignVersion")
-    async def list_all_draw_designs(self, client_date_time: _datetime.datetime = None, design_version: int = None) -> _List[_DrawDesign]:
-        production_server = await self.get_production_server()
-        result = await _CharacterServiceRaw.list_all_draw_designs(production_server, _utils.datetime.convert_to_pss_timestamp(client_date_time), design_version, self.language_key)
-        return result
+    async def list_all_draw_designs(
+        self,
+        client_date_time: _datetime.datetime = None,
+        design_version: int = None,
+    ) -> _List[_DrawDesign]:
+        params = {
+            "clientDateTime": _utils.datetime.convert_to_pss_timestamp(client_date_time),
+            "languageKey": str(self.language_key),
+        }
+        if design_version is not None:
+            params["designVersion"] = design_version
+
+        response = await self.client.raw.call(
+            "CharacterService",
+            "ListAllDrawDesigns",
+            params=params,
+            use_cache=False,
+        )
+        return _service_base.parse_entity_list(response, "DrawDesigns", _DrawDesign)
 
     async def prestige_character_from(self, character_design_id: int) -> _List[_Prestige]:
-        production_server = await self.get_production_server()
-        result = await _CharacterServiceRaw.prestige_character_from(production_server, character_design_id)
-        return result
+        response = await self.client.raw.call(
+            "CharacterService",
+            "PrestigeCharacterFrom",
+            params={"characterDesignId": character_design_id},
+            use_cache=False,
+        )
+        return _service_base.parse_entity_list(response, "Prestiges", _Prestige)
 
     async def prestige_character_to(self, character_design_id: int) -> _List[_Prestige]:
-        production_server = await self.get_production_server()
-        result = await _CharacterServiceRaw.prestige_character_to(production_server, character_design_id)
-        return result
+        response = await self.client.raw.call(
+            "CharacterService",
+            "PrestigeCharacterTo",
+            params={"characterDesignId": character_design_id},
+            use_cache=False,
+        )
+        return _service_base.parse_entity_list(response, "Prestiges", _Prestige)
